ab09f7cd85b38cd96f0c838b25add781fb987228
Jorge Santos <jsantossimon@gmail.com>
2013-05-09 14:04:51 +0900

    Execute with hifh priority to avoid out of sync problems

--- scripts/ir_sensors.bash
+++ scripts/ir_sensors.bash
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-sleep 60
-rosrun rosserial_python serial_node.py /dev/arduino
+#sleep 40
+#rosrun rosserial_python serial_node.py /dev/arduino
+
+#sudo su -
+#source /home/turtlebot/cafe_solution/devel/setup.bash
+nice -n -11 rosrun rosserial_python serial_node.py /dev/arduino
+#exit
+#sudo su - turtlebot
+# si de verdad funciona, hacerlo bien   @sudo ...  o  preguntar a daniel
+
+
+##########   asi no va; error:  -su: no job control in this shell
