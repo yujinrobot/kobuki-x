737326cb5c60deaf843c4d8d48825550f86f1474
Jorge Santos <jsantossimon@gmail.com>
2013-05-13 08:57:11 +0900

    Recuperate the sleep and increase the priority, as it still fails sometimes

--- scripts/ir_sensors.bash
+++ scripts/ir_sensors.bash
@@ -1,14 +1,4 @@
 #!/bin/bash
 
-#sleep 40
-#rosrun rosserial_python serial_node.py /dev/arduino
-
-#sudo su -
-#source /home/turtlebot/cafe_solution/devel/setup.bash
-nice -n -11 rosrun rosserial_python serial_node.py /dev/arduino
-#exit
-#sudo su - turtlebot
-# si de verdad funciona, hacerlo bien   @sudo ...  o  preguntar a daniel
-
-
-##########   asi no va; error:  -su: no job control in this shell
+sleep 30
+nice -n -16 rosrun rosserial_python serial_node.py /dev/arduino
