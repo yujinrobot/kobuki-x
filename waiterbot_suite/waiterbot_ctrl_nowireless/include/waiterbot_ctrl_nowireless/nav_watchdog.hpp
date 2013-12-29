453864aaf140b02545fb437427495f3c6b82248d
Jihoon Lee <jihoonlee.in@gmail.com>
2013-12-29 16:50:03 +0900

    ar_marker processor initial

---
+++ waiterbot_suite/waiterbot_ctrl_nowireless/include/waiterbot_ctrl_nowireless/nav_watchdog.hpp
@@ -0,0 +1,16 @@
+/*
+  Nav Watchdog Class
+
+  Highly inspired by nav_watchdog from waiterbot_controller written by Jorge Santos Simon
+  It watches localization and relocalize based on global ar markers
+
+  LICENSE : BSD - https://raw.github.com/yujinrobot/kobuki-x/license/LICENSE
+
+  Author :  Jihoon Lee
+  Date   : Dec 2013
+ */
+
+#ifndef _WAITER_NODE_NAV_WATCHDOG_HPP_
+#define _WAITER_NODE_NAV_WATCHDOG_HPP_
+
+#include <ros/ros.h>
