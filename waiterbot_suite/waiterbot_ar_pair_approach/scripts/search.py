e3a527e5f3495158be608fd4c8a01f77688becda
Daniel Stonier <d.stonier@gmail.com>
2014-01-07 01:15:24 +0900

    waiterbot_ar_pair_search_behaviour -> waiterbot_ar_pair_approach.

--- waiterbot_suite/waiterbot_ar_pair_approach/scripts/search.py
+++ waiterbot_suite/waiterbot_ar_pair_approach/scripts/search.py
@@ -4,10 +4,10 @@
 #   https://raw.github.com/yujinrobot/kobuki-x/license/LICENSE
 #
 import rospy
-import waiterbot_ar_pair_search_behaviour
+import waiterbot_ar_pair_approach
 
 if __name__ == '__main__':
 
     rospy.init_node('ar_pair_search')
-    search_behaviour = waiterbot_ar_pair_search_behaviour.Node()
+    search_behaviour = waiterbot_ar_pair_approach.Node()
     search_behaviour.spin()
