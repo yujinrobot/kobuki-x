822c9f9846bf41c227b0422aee7a6de037d0051e
Marcus Liebhardt <ich@marcusliebhardt.de>
2014-01-06 12:55:04 +0900

    waiterbot_ctrl_nowireless: adds parameterisation of vm nav goal

--- waiterbot_suite/waiterbot_ctrl_nowireless/include/waiterbot_ctrl_nowireless/waiter_node.hpp
+++ waiterbot_suite/waiterbot_ctrl_nowireless/include/waiterbot_ctrl_nowireless/waiter_node.hpp
@@ -80,6 +80,7 @@ namespace waiterbot {
       std::string base_frame_;
       std::string odom_frame_;
       std::string global_frame_;
+      std::string nav_target_vm_;
 
       bool initialized_;
       bool waypointsReceived_;
